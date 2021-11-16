370971d5a5a9d51c20c962901debcc9b164432c2
11SF <nsfolknsf@gmail.com>
2021-11-16 10:19:18 +0700

    first commit

---
+++ src/__test__/math.test.js
@@ -0,0 +1,19 @@
+const {add, subtract, multiply} = require("../math");
+
+describe("simple arithmetic", () => {
+  describe("addition", () => {
+    test("expect 5 + 3 = 8", () => {
+      expect(add(5, 3)).toEqual(8);
+    });
+  });
+  describe("subtract", () => {
+    test("expect 5 - 1 = 4", () => {
+      expect(subtract(5, 1)).toEqual(4);
+    });
+  });
+  describe("multiply", () => {
+    test("expect 2 * 3 = 6", () => {
+      expect(multiply(2, 3)).toEqual(6);
+    });
+  });
+});
